d5e6fa7e84e2ded62743b7f52bdeced119c3aa66
UO282208 <UO282208@uniovi.es>
2024-09-09 18:03:51 +0200

    Modified backend to support angular (Not working)

--- backend/src/main/java/com/example/application/Application.java
+++ backend/src/main/java/com/example/application/Application.java
@@ -1,10 +1,15 @@
 package com.example.application;
 
+import java.util.Arrays;
+
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.example.application.imageupload.storage.StorageProperties;
 import com.example.application.imageupload.storage.StorageService;
@@ -23,5 +28,21 @@ public class Application {
 			storageService.deleteAll();
 			storageService.init();
 		};
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
+			"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST"));
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter(urlBasedCorsConfigurationSource);
 	}
 }
--- backend/src/main/java/com/example/application/imageupload/FileUploadController.java
+++ backend/src/main/java/com/example/application/imageupload/FileUploadController.java
@@ -1,28 +1,28 @@
 package com.example.application.imageupload;
 
-import java.io.IOException;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.application.imageupload.storage.StorageFileNotFoundException;
 import com.example.application.imageupload.storage.StorageService;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class FileUploadController{
 
 	private final StorageService storageService;
@@ -32,18 +32,18 @@ public class FileUploadController{
 		this.storageService = storageService;
 	}
 
-	@GetMapping("/")
-	public String listUploadedFiles(Model model) throws IOException {
+	@GetMapping("/allFiles")
+	@ResponseBody
+	public ResponseEntity<List<String>> getFiles() {
 
-		model.addAttribute("files", storageService.loadAll().map(
-				path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
-						"serveFile", path.getFileName().toString()).build().toUri().toString())
-				.collect(Collectors.toList()));
-
-		return "uploadForm";
+		List<String> files = storageService.loadAll().map(
+            path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                    "serveFile", path.getFileName().toString()).build().toUri().toString())
+            .collect(Collectors.toList());
+        return ResponseEntity.ok(files);
 	}
 
-	@GetMapping("/files/{filename:.+}")
+	@GetMapping("/{filename:.+}")
 	@ResponseBody
 	public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
 
@@ -56,15 +56,12 @@ public class FileUploadController{
 				"attachment; filename=\"" + file.getFilename() + "\"").body(file);
 	}
 
-	@PostMapping("/")
-	public String handleFileUpload(@RequestParam("file") MultipartFile file,
-			RedirectAttributes redirectAttributes) {
+	@PostMapping("/uploadFile")
+	public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
 
 		storageService.store(file);
-		redirectAttributes.addFlashAttribute("message",
-				"You successfully uploaded " + file.getOriginalFilename() + "!");
 
-		return "redirect:/";
+		return ResponseEntity.ok("File uploaded successfully");
 	}
 
 	@ExceptionHandler(StorageFileNotFoundException.class)
