469b1c743f747b30a137a5a5213f5d3127868e10
UO282208 <UO282208@uniovi.es>
2024-10-25 16:06:45 +0200

    Codigo más limpio

--- backend/src/main/java/com/example/application/imageupload/storage/StorageProperties.java
+++ backend/src/main/java/com/example/application/imageupload/storage/StorageProperties.java
@@ -2,28 +2,15 @@ package com.example.application.imageupload.storage;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @ConfigurationProperties("storage")
 public class StorageProperties {
 
-	/**
-	 * Folder location for storing files
-	 */
 	private String location = "storefiles/";
 	private String resultsLocation = "storefiles/results";
 
-	public String getLocation() {
-		return location;
-	}
-
-	public void setLocation(String location) {
-		this.location = location;
-	}
-
-	public String getResultsLocation() {
-		return resultsLocation;
-	}
-
-	public void setResultsLocation(String resultsLocation) {
-		this.resultsLocation = resultsLocation;
-	}
 }
